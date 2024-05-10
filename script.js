0f095f24192b6dce910d657ad8e44aa9fb9b8613
Sharan Yalburgi <sharanyalburgi@gmail.com>
2024-05-11 00:52:19 +0530

    Allow system prompts

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Global variable for the base URL
 var baseUrl = 'http://localhost:11434';
+varmessages = [];
 
 // Function to set the base URL
 function setBaseUrl(url) {
@@ -21,31 +22,35 @@ function fetchAvailableModels() {
     
 }
 
-function submitPrompt(model, prompt) {
+function submitPrompt(model, system, prompt) {
     if (!model) {
         return Promise.reject('Model not specified.');
     }
 
+    if (!system) {
+        return Promise.reject('System prompt not specified.');
+    }
+
     if (!prompt) {
-        return Promise.reject('Prompt not specified.');
+        return Promise.reject('User Prompt not specified.');
     }
 
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ model, prompt })
+        body: JSON.stringify({ 
+            model, 
+            messages: [
+                { role: 'system', content: system },
+                { role: 'user', content: prompt }
+            ]
+        })
         // stream: false
     };
 
-    return fetch(`${baseUrl}/api/generate`, requestOptions)
-        // .then(response => response.json())
-        // .then(data => {
-        //     return data.response;
-        // })
+    return fetch(`${baseUrl}/api/chat`, requestOptions)
         .catch(error => {
             console.error('Error submitting prompt:', error);
             throw new Error('Failed to get response from the server.');
         });
 }
-
-
